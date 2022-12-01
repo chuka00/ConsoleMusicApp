30a73dd69bff19ae237b313e61b860a4c2891337
Victor Chukwuka Ogbodo <chukaogbodo7@gmail.com>
2022-12-01 23:27:57 +0100

    Little Updates and corrections

--- ConsoleMusicApp/ApplicationOne.cs
+++ ConsoleMusicApp/ApplicationOne.cs
@@ -2,6 +2,15 @@
 {
     internal class ApplicationOne
     {
+
+        public static string? PlayListName;
+
+        private static readonly Random rng = new();
+
+        public static List<Tracklist> list = new();
+
+        public static int myid = 0;
+
         public static List<Tracklist> tracks = new List<Tracklist>{
             new Tracklist {Id = 1, Track ="Ms Jackson", Artist ="Outkast" },
             new Tracklist {Id = 2, Track ="Hey ya", Artist ="Outkast" },
@@ -16,7 +25,7 @@
         {
             Console.WriteLine("\n*----------Pineapple Music----------*\n What would you like to do?");
             Console.WriteLine("\n1. Show Current Tracklist\n2. Add to Tracklist\n3. Remove from Playlist" +
-                "\n4. Edit Track from Playlist\n5. Shuffle Tracklist\n6. Arrange Tracklist");
+                "\n4. Edit Track from Playlist\n5. Shuffle Tracklist\n6. Arrange Tracklist\n7. Create a new playlist");
             NextAction();
         }
 
@@ -44,6 +53,12 @@
                 case 6:
                     ArrangeTracklist();
                     break;
+                case 7:
+                    NewPlaylist.CreatedPlaylist();
+                    break;
+                case 8:
+                    Environment.Exit(100);
+                    break;
                 default:
                     Console.WriteLine("Incorrect Input, Try Again");
                     break;
@@ -66,6 +81,8 @@
 
         public static void AddToTracklist()
         {
+            try
+            {
             Console.WriteLine("Input Song to be Added to the playlist\nAdd Song Name");
             var addedSong = Console.ReadLine();
             Console.WriteLine("Add the Artist Name");
@@ -73,20 +90,41 @@
             tracks.Add(new Tracklist { Artist = addedArtist, Track = addedSong, Id = tracks.Last().Id + 1 });
             ShowPlayList();
             WelcomeMsg();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid");
+            }
         }
 
         public static void RemoveFromTracklist()
         {
+            try
+            {
             GetMusic().ForEach(music => Console.WriteLine($"Music id {music.Id} Track name: {music.Track} Artist: {music.Artist}"));
             Console.WriteLine("\nEnter music ID to remove");
             int removetrack = Convert.ToInt32(Console.ReadLine());
             var firstMatch = GetMusic().First(s => s.Id == removetrack);
             GetMusic().Remove(firstMatch);
             ShowPlayList();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid");
+            }
         }
 
         public static void EditTracklist()
         {
+            try { 
             GetMusic().ForEach(music => Console.WriteLine($"Music id {music.Id} Track name: {music.Track} Artist: {music.Artist}"));
             Console.WriteLine("\nEnter music ID to Edit");
             int trackId = Convert.ToInt32(Console.ReadLine());
@@ -105,10 +143,20 @@
             Console.WriteLine("Sucessfully updated");
 
             ShowPlayList();
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid");
+            }
         }
 
         public static void ShuffleTracklist()
         {
+            try { 
             int lastIndex = tracks.Count() - 1;
             while (lastIndex > 0)
             {
@@ -118,6 +166,15 @@
                 tracks[randomTrack] = tempValue;
                 lastIndex--;
                 ShowPlayList();
+            }
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid");
             }
         }
         public static void ArrangeTracklist()
---
+++ ConsoleMusicApp/NewPlaylist.cs
@@ -0,0 +1,63 @@
+﻿namespace ConsoleMusicApp
+{
+    internal class NewPlaylist
+    {
+        public static void CreatedPlaylist()
+        {
+            Console.Clear();
+            try
+            {
+                Console.WriteLine("Enter new playlist name");
+                ApplicationOne.PlayListName = Console.ReadLine();
+
+                Console.WriteLine("{0} was successfully Created", ApplicationOne.PlayListName.ToUpper());
+
+                bool checkplay = true;
+                while (checkplay)
+                {
+                    Console.WriteLine("Enter new Id");
+                    int enterID = Convert.ToInt32(Console.ReadLine());
+
+                    foreach (var msc in ApplicationOne.tracks)
+                    {
+                        if (enterID == msc.Id)
+                        {
+
+                            ApplicationOne.list.Add(msc);
+                            ApplicationOne.list.ForEach(newplaylist => Console.WriteLine($"{newplaylist.Id} {newplaylist.Track}"));
+
+                        }
+
+
+                    }
+                    Console.WriteLine("Press 1 to add more \n Press any key to exit");
+                    var moremusic = Console.ReadLine();
+                    if (moremusic == "1")
+                    {
+                        checkplay = true;
+                    }
+                    else
+                    {
+                        Environment.Exit(100);
+                        break;
+                    }
+
+
+
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
